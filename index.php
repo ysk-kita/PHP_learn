82bff580949f1970fab6455364a7745d52ac6655
yu-kitano <ktn.ysk.1996@gmail.com>
2019-03-12 07:57:05 +0900

    hello world

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <b>
+            <?php
+                print "hello world"
+            ?>
+        </b>
+    </body>
+</html>
