a313bfd6d029f92f72e59cdd1a2b651453a21b77
yu-kitano <ktn.ysk.1996@gmail.com>
2019-03-18 08:13:24 +0900

    簡単な計算器っぽいものを作ってみる part2 なんかこれじゃない感… 今後の課題は動的に対応すること。 基礎進めてからこの機能を更新する

--- calc.php
+++ calc.php
@@ -5,19 +5,20 @@
         array("豚肉", 200)
     );
     $i = 0;
+    $j = 0;
     session_start();
     $_SESSION['array'] = $table_data;
 ?>
     
 <html>
-    <head><title>$title</title></head>
+    <head><title>calc input</title></head>
     <body>
         <form action="calc_confirm.php" method="POST" >
             <table>
                 <thead>
                     <tr>
                         <th>商品名</th>
-                        <th>値段  </th>
+                        <th>値段(円)</th>
                         <th>数量  </th>
                     </tr>
                 </thead>
@@ -26,7 +27,9 @@
                     <tr>
                         <?php foreach($row as $cel): ?>
                             <td><?= $cel ?></td>
-                        <?php endforeach; ?>
+                        <?php
+                            $j++;
+                            endforeach; ?>
                         <td><input type="number" name=<?php echo 'quantity'.$i; ?> /></td>
                     </tr>
                 <?php 
--- calc_confirm.php
+++ calc_confirm.php
@@ -1,11 +1,14 @@
 <html>
-    <head><title>$title</title></head>
+    <head><title>calc_confirm</title></head>
     <body>
         <?php 
             session_start();
-            $quan1 = $_POST['quantity1'];
-            print $quan1;
-            print $_SESSION['array'][0][0];
+            $quan1 = $_POST["quantity0"];
+            $price1 = $_SESSION["array"][0][1];
+            print nl2br($_SESSION["array"][0][0]."の会計は\n");
+            print nl2br("・値段：".$price1."\n");
+            print nl2br("・数量：".$quan1."\n");
+            print nl2br("より".strval(intval($quan1)*intval($price1))."円です。");
         ?>
     </body>
 </html>    
