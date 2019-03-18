89cc12e9e80dbf8347dff7a78789100e108bce14
yu-kitano <ktn.ysk.1996@gmail.com>
2019-03-19 08:28:46 +0900

    phpベースで変数ありのページ作成 # htmlベースで<?php?>とするべきか、phpベースでprintを使ってhtmlとするべきか…

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
         </form>
         <label><a href="sample1.php">sample1</a></label><br/>
         <label><a href="sample2.php">sample2</a></label><br/>
-        <label><a href="calc.php">sample3</a></label><br/>
+        <label><a href="sample3.php">sample3</a></label><br/>
+        <label><a href="calc.php">calc</a></label><br/>
     </body>
 </html>
---
+++ sample3.php
@@ -0,0 +1,10 @@
+<?php 
+    print "<select name='people'>";
+    $i=0;
+    while($i<10){
+        print "<option class='{$i}hoge'>huga</option>";
+        $i++;
+    }
+    print "</select>";
+?>
+
