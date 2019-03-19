c7ee8b09ce7439cb7d380d63184e29910dd4aa0b
yu-kitano <ktn.ysk.1996@gmail.com>
2019-03-20 08:04:16 +0900

    配列でtableを作ってみる。 if制御しなければ全部<tbody>属性の下に格納される？
    # phpの配列ってpythonのDictと近い…？　
    # 配列キー未指定なら他言語のArrayそのままだけど…

--- sample3.php
+++ sample3.php
@@ -6,5 +6,23 @@
         $i++;
     }
     print "</select>";
+
+    $array = array('key' => 'value', 'usr00' => 'Super User', 'usr01' => 'General User');
+    print "<table> \n";
+    // 配列のkeyをループで取り出し、そのKeyに対応するvalueも取り出す
+    $i = 0;
+    foreach ($array as $key => $value){
+        if($i == 0){
+            print "<thead><tr><th>$key</th><th>$value</th></tr></thead> \n";
+            print "<tbody> \n";
+        } else {
+            print "<tr><td>$key</td><td>$value</td></tr> \n";
+        }
+        
+        $i++;
+    }
+    print "</tbody> \n";
+    print "</table>";
 ?>
+
 
