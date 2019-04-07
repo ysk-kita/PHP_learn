484d9472453e66265a6440065eac01eedfff38bb
yu-kitano <ktn.ysk.1996@gmail.com>
2019-04-07 15:08:42 +0900

    sql接続し、sql発行

--- dbAccess.php
+++ dbAccess.php
@@ -1,7 +1,17 @@
 <?php
     try{
-        // データベースへ接続
-        $pdo = new PDO("mysql:host=127.0.0.1;port=3306;dbname=test;charset=utf8", "root", "");
+        // dbNameで指定したデータベース(スキーマ)にアクセスする。
+        $mysqlPdo = new PDO("mysql:host=127.0.0.1; port=3306; dbname=Schema190417; charset=utf8", "root", "");
+        
+        // sql実行エラーが起きた場合の対応に「ERRMODE_EXCEPTION」(例外をスローする)を指定
+        $mysqlPdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        // sqlファイル読み込み
+        $sql1 = file_get_contents("sql/create_dishes.sql");
+
+        // sql実行
+        $mysqlPdo->exec($sql1);
+        
     } catch (PDOException $e){
         print('Error:'.$e->getMessage());
         die();
---
+++ sql/create_dishes.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS dishes (
+    dish_id INTEGER PRIMARY KEY,
+    dish_name VARCHAR(50),
+    price DECIMAL(4,2)    
+);
